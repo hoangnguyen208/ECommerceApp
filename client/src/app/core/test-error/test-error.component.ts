5668caef5315d14f576cfca8f386e49b7c522720
hoangnguyen.vn208@gmail.com <hoangnguyen.vn208@gmail.com>
2020-05-09 14:47:51 +0300

    added client error handling

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  validationErrors: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/fortytwo').subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+      this.validationErrors = err.errors;
+    });
+  }
+
+}
