7ba6af81cf1a3dfa3c3ff6aa3fc02c1b850fd60b
hoangnguyen.vn208@gmail.com <hoangnguyen.vn208@gmail.com>
2020-05-10 16:30:21 +0300

    improves client with breadcrumb, loading indicator, and other stylings

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+    this.bcService.set('@productDetails', '');
+  }
 
   ngOnInit() {
     this.loadProduct();
@@ -20,9 +23,9 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(res => {
       this.product = res;
+      this.bcService.set('@productDetails', res.name);
     }, err => {
       console.error(err);
     });
   }
-
 }
