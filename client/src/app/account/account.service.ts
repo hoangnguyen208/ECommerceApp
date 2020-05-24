91782b6611c53a8e0c278ac84a29f3886ae07b5d
hoangnguyen.vn208@gmail.com <hoangnguyen.vn208@gmail.com>
2020-05-24 18:30:12 +0300

    added authorization client side

---
+++ client/src/app/account/account.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, ReplaySubject, of } from 'rxjs';
+import { IUser } from '../shared/models/user';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<IUser>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  loadCurrentUser(token: string) {
+    if (token === null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+    return this.http.get(this.baseUrl + 'account', { headers }).pipe(
+      map((res: IUser) => {
+        if (res) {
+          localStorage.setItem('token', res.token);
+          this.currentUserSource.next(res);
+        }
+      })
+    );
+  }
+
+  login(values: any) {
+    return this.http.post(this.baseUrl + 'account/login', values).pipe(
+      map((res: IUser) => {
+        if (res) {
+          localStorage.setItem('token', res.token);
+          this.currentUserSource.next(res);
+        }
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.http.post(this.baseUrl + 'account/register', values).pipe(
+      map((res: IUser) => {
+        if (res) {
+          localStorage.setItem('token', res.token);
+          this.currentUserSource.next(res);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string) {
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+}
